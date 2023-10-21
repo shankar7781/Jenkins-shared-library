578b76c293bb4659c0edc3fd0793d0950d8d2479
shankar7781 <109841139+shankar7781@users.noreply.github.com>
2023-10-21 15:14:01 +0530

    Update dockerImageScan.groovy

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,5 +1,5 @@
 def call(String project, String ImageTag , String hubUser){
   sh """ 
-    trivy image ${hubUser}/${project} ${hubUser}/${project}:latest
+    trivy image ${hubUser}/${project} ${hubUser}/${project}:latest > scan.txt
   """
 }
